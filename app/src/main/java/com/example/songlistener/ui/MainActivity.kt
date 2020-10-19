78ca148665ddcf0d4dd596693217572edcf62c77
Tomasz Białek <bialek.tomasz225@gmail.com>
2020-10-19 20:46:39 +0200

    PlayState

--- app/src/main/java/com/example/songlistener/ui/MainActivity.kt
+++ app/src/main/java/com/example/songlistener/ui/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.songlistener.ui
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
 import com.example.songlistener.R
 import com.example.songlistener.adapter.SwipeSongAdapter
 import com.example.songlistener.data.entity.Song
+import com.example.songlistener.exoplayer.isPlaying
 import com.example.songlistener.exoplayer.toSong
 import com.example.songlistener.other.Status.*
 import com.example.songlistener.ui.viewmodels.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -27,12 +31,31 @@ class MainActivity : AppCompatActivity() {
 
     private var currentPlayingSong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         subscribeToObservers()
 
         vpSong.adapter = swipeSongAdapter
+
+        vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (playbackState?.isPlaying == true) {
+                    mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+                } else {
+                    currentPlayingSong = swipeSongAdapter.songs[position]
+                }
+            }
+        })
+
+        ivPlayPause.setOnClickListener {
+            currentPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
     }
 
     private fun switchViewPagerToCurrentSong(song: Song) {
@@ -45,8 +68,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun subscribeToObservers() {
         mainViewModel.mediaItems.observe(this, {
-            it?.let {result ->
-                when(result.status) {
+            it?.let { result ->
+                when (result.status) {
                     SUCCESS -> {
                         result.data?.let { songs ->
                             swipeSongAdapter.songs = songs
@@ -68,6 +91,39 @@ class MainActivity : AppCompatActivity() {
             currentPlayingSong = it.toSong()
             glide.load(currentPlayingSong?.imageUrl).into(ivCurSongImage)
             switchViewPagerToCurrentSong(currentPlayingSong ?: return@observe)
+        })
+
+        mainViewModel.playbackState.observe(this, {
+            playbackState = it
+            ivPlayPause.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+        })
+
+        mainViewModel.isConnected.observe(this, {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        rootLayout,
+                        result.message ?: "An unknown error occurred",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    else -> Unit
+                }
+            }
+        })
+
+        mainViewModel.networkError.observe(this, {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        rootLayout,
+                        result.message ?: "An unknown error occurred",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    else -> Unit
+                }
+            }
         })
     }
 }
